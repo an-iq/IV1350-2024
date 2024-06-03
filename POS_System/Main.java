fe45c04a8e3278692df9bc1c6041de7fe54c2940
Aniqa <an-iq@hotmail.com>
2024-06-03 17:36:28 +0200

    3

--- POS_System/Main.java
+++ POS_System/Main.java
@@ -1,13 +1,16 @@
-package pos;
+package POS_System;
+import POS_System.src.controller.Controller;
+import POS_System.src.integration.AccountingSystem;
+import POS_System.src.integration.InventorySystem;
+import POS_System.src.view.View;
 
-/**
- * Hello world!
- *
- */
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Main {
+    public static void main(String[] args) {
+        InventorySystem inventorySystem = new InventorySystem();
+        AccountingSystem accountingSystem = new AccountingSystem();
+        Controller controller = new Controller(inventorySystem, accountingSystem);
+        View view = new View(controller);
+
+        view.executeSale();
     }
 }
